c57979d5a376ca033bf4d453563dd54a2f58f612
Kalucza Gábor <kaluczadzsi@gmail.com>
2024-04-01 13:44:36 +0200

    TDD development

--- src/components/Greet.test.tsx
+++ src/components/Greet.test.tsx
@@ -4,6 +4,12 @@ import { render, screen } from '@testing-library/react';
 
 test('Greet renders correctly', () => {
   render(<Greet />);
-  const textElement = screen.getByText(/hello/i);
+  const textElement = screen.getByText('Hello');
+  expect(textElement).toBeInTheDocument();
+});
+
+test('Greet renders with a name', () => {
+  render(<Greet name="Mike" />);
+  const textElement = screen.getByText('Hello Mike');
   expect(textElement).toBeInTheDocument();
 });
