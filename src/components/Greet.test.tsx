e08c2b81e2c8dbd63a552cdbd5df4b30a34949d5
Kalucza Gábor <kaluczadzsi@gmail.com>
2024-04-01 13:59:37 +0200

    grouping tests

--- src/components/Greet.test.tsx
+++ src/components/Greet.test.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import Greet from './Greet';
 import { render, screen } from '@testing-library/react';
 
-test('Greet renders correctly', () => {
-  render(<Greet />);
-  const textElement = screen.getByText('Hello');
-  expect(textElement).toBeInTheDocument();
+describe('Greet', () => {
+  test('renders correctly', () => {
+    render(<Greet />);
+    const textElement = screen.getByText('Hello');
+    expect(textElement).toBeInTheDocument();
+  });
 });
 
-test('Greet renders with a name', () => {
-  render(<Greet name="Mike" />);
-  const textElement = screen.getByText('Hello Mike');
-  expect(textElement).toBeInTheDocument();
+describe('Nested', () => {
+  test('renders with a name', () => {
+    render(<Greet name="Mike" />);
+    const textElement = screen.getByText('Hello Mike');
+    expect(textElement).toBeInTheDocument();
+  });
 });
