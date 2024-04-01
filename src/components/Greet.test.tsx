e32b9ef8983aef81cf096272adee016afaaeeb5f
Kalucza Gábor <kaluczadzsi@gmail.com>
2024-04-01 13:36:34 +0200

    first test

---
+++ src/components/Greet.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Greet from './Greet';
+import { render, screen } from '@testing-library/react';
+
+test('Greet renders correctly', () => {
+  render(<Greet />);
+  const textElement = screen.getByText(/hello/i);
+  expect(textElement).toBeInTheDocument();
+});
