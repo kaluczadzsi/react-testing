95ab2321bfec9ebdd3197690c1dc3470e763e8b2
Kalucza Gábor <kaluczadzsi@gmail.com>
2024-04-03 10:47:44 +0200

    getbyplaceholdertext

--- src/components/greet/Greet.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { GreetProps } from "./greet.type";
-
-const Greet = ({ name }: GreetProps) => {
-  return <div>Hello {name ? name : "Guest"}</div>;
-};
-
-export default Greet;
